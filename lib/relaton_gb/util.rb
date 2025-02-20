4169f445fe4c01be37e39789a0929e0ac8186ef0
Andrei Kislichenko <android.2net@gmail.com>
2025-02-20 18:32:24 -0500

    WIP create flavor model

--- lib/relaton_gb/util.rb
+++
@@ -1,6 +0,0 @@
-module RelatonGb
-  module Util
-    extend RelatonBib::Util
-    PROGNAME = "relaton-gb".freeze
-  end
-end
--- spec/relaton_gb/document_type_spec.rb
+++
@@ -1,7 +0,0 @@
-describe RelatonGb::DocumentType do
-  it "warns if invalid doctype" do
-    expect do
-      RelatonGb::DocumentType.new type: "invalid"
-    end.to output(/\[relaton-gb\] WARN: invalid doctype: `invalid`/).to_stderr_from_any_process
-  end
-end
